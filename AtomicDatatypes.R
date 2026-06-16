5ab5767be3fdf29b6fc7586dced9c76aa0e1bcca
Man J Gajjar <131265476+ManJGajjar@users.noreply.github.com>
2026-06-16 11:13:05 +0530

    Add files via upload

---
+++ AtomicDatatypes.R
@@ -0,0 +1,27 @@
+# Atomic Datatype in R includes -> Numeric, Integer, Logical, Character, Date/Time
+
+#Numeric Datatype
+temperature <- 40.0
+print(temperature)
+
+#Integer Datatype
+age <- 30
+print(age)
+
+#Logical Datatype
+is_warm <- TRUE
+print(is_warm)
+
+#Character Datatype
+greeting <- "Hello, Peter Griffin !"
+print(greeting)
+
+#Date/Time Datatype
+current_date <- sys.Date()
+print(current_date)
+
+# O/P - [1] 40
+# O/P - [1] 30
+# O/P - [1] TRUE
+# O/P - [1] "Hello, Peter Griffin !"
+# O/P - [1] "2026-06-12"
