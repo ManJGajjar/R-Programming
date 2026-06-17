d5f02f8f8175dcc87223db568ee42532fb709438
Man J Gajjar <131265476+ManJGajjar@users.noreply.github.com>
2026-06-17 20:17:45 +0530

    Add files via upload

---
+++ ArithmeticOperators.R
@@ -0,0 +1,20 @@
+#Arithmetic Operators in R
+
+a <- 10
+b <- 20
+
+print(a + b) # Addition
+print(a - b) # Subtraction
+print(a * b) # Multiplication
+print(a / b) # Division
+print(a %% b) # Modulus
+print(a %/% b) # Integer Division
+print(a ^ b) # Exponentiation
+
+# O/P - [1] 30
+# O/P - [1] -10
+# O/P - [1] 200
+# O/P - [1] 0.5
+# O/P - [1] 10
+# O/P - [1] 0
+# O/P - [1] 100000000000000000000
