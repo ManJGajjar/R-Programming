b07bdc5058f931e839abac9485e1639364034007
Man J Gajjar <131265476+ManJGajjar@users.noreply.github.com>
2026-06-19 22:55:54 +0530

    Add files via upload

---
+++ ComparisonOperator.R
@@ -0,0 +1,18 @@
+# Comparison Operators in R
+
+a <- 10
+b <- 20
+
+print(a > b) # Greater than
+print(a < b) # Less than
+print(a == b) # Equal to
+print(a != b) # Not equal to
+print(a >= b) # Greater than or equal to
+print(a <= b) # Less than or equal to
+
+# O/P - [1] FALSE
+# O/P - [1] TRUE
+# O/P - [1] FALSE
+# O/P - [1] TRUE
+# O/P - [1] FALSE
+# O/P - [1] TRUE
