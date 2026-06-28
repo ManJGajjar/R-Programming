71b2461b68c04bde976e94c4cfeba4d588d41997
Man J Gajjar <131265476+ManJGajjar@users.noreply.github.com>
2026-06-28 10:09:33 +0530

    Add files via upload

---
+++ Vectors.R
@@ -0,0 +1,17 @@
+# Vectors in R
+
+# Creating a numeric vector
+numeric_vector <- c(1, 2, 3, 4, 5)
+print(numeric_vector)
+
+# Creating a character vector
+character_vector <- c("apple", "banana", "cherry")
+print(character_vector)
+
+# Creating a logical vector
+logical_vector <- c(TRUE, FALSE, TRUE, FALSE)
+print(logical_vector)
+
+# O/P - [1] 1 2 3 4 5
+# O/P - [1] "apple" "banana" "cherry"
+# O/P - [1] TRUE FALSE TRUE FALSE
