45e861c75980a17a9c623436e7b0c19ca19488ec
Man J Gajjar <131265476+ManJGajjar@users.noreply.github.com>
2026-06-29 19:25:01 +0530

    Add files via upload

---
+++ VectorizedOperations.R
@@ -0,0 +1,10 @@
+# Vectorized Operations in R
+
+deposit <- c(1000, 2000, 3000)
+interest_rate <- 0.05
+
+# Calculate interest for each deposit using vectorized operations
+interest <- deposit * interest_rate
+print(interest) 
+
+# O/P - [1]  50 100 150
