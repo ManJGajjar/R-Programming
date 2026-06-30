380dd64300af59e028f07ad1de768bfadf0f6bfc
Man J Gajjar <131265476+ManJGajjar@users.noreply.github.com>
2026-06-30 23:12:54 +0530

    Add files via upload

---
+++ Matrices.R
@@ -0,0 +1,9 @@
+# Matrices in R
+
+matrix_data <- matrix(1:9, nrow = 3, ncol = 3)
+print(matrix_data)
+
+# O/P -      [,1] [,2] [,3]
+# O/P - [1,]    1    4    7
+# O/P - [2,]    2    5    8
+# O/P - [3,]    3    6    9
