b6748e1a517635198240909a681a44c8d3e7e0fa
Man J Gajjar <131265476+ManJGajjar@users.noreply.github.com>
2026-07-02 11:52:29 +0530

    Add files via upload

---
+++ DataFrames.R
@@ -0,0 +1,14 @@
+# DataFrames in R
+
+students <- data.frame(
+  Name = c("Alice", "Bob", "Charlie"),
+  Age = c(20, 22, 21),
+  Grade = c("A", "B", "A")
+)
+
+print(students)
+
+# O/P -      Name Age Grade
+# O/P - 1   Alice  20     A
+# O/P - 2     Bob  22     B
+# O/P - 3 Charlie  21     A
