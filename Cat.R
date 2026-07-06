aed82890e73bd33e2508d4b9b9cb00b2df7a1f92
Man J Gajjar <131265476+ManJGajjar@users.noreply.github.com>
2026-07-06 11:34:11 +0530

    Add files via upload

---
+++ Cat.R
@@ -0,0 +1,16 @@
+# Cat is used to concatenate strings in R
+
+a <- 10
+b <- 20
+c <- 30
+
+print(a)
+print(b)
+print(c)
+
+cat("The values are ", a, b, c, "\n")
+
+# O/P - [1] 10
+# O/P - [1] 20
+# O/P - [1] 30
+# O/P - The values are  10 20 30
